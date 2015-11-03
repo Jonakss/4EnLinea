de9e42b6af56fb7dca9495b7fc99466b99856521
Jonathan Correa Paiva <jona.correapc@gmail.com>
2015-11-02 23:19:17 -0300

    Added key input handler

--- 4enlinea.pas
+++ 4enlinea.pas
@@ -14,11 +14,38 @@ type
 var 
 	gameOver:Boolean;
 	Tablero: TTable;
-	i, j: Integer;
+	i, j, x: Integer;
+	var a: char;
+
+Procedure KeyScan;
+BEGIN
+If KeyPressed then
+    BEGIN
+      a := ReadKey;
+      case a of
+      	#75:  {LEFT}
+      	begin
+      		if x = 0 then
+      			x:=0
+      		else
+      			x:=x-1
+      	end;
+      	#77:  {RIGTH}
+		begin
+	      	if x = ANCHO then
+      			x:=ANCHO
+      		else
+      			x:=x+1	
+		end;
+      end;
+    END
+END;
 
 procedure init;
 begin
+	a:=ESPACIO;
 	clrscr;
+	x:=0;
 	gameOver := false;	
 	{Llenar array del tablero}
 	for i := 1 to ANCHO do
@@ -30,16 +57,20 @@ begin
 	end;
 end;
 
+
 procedure dibujarTablero;
 begin
 	for i := 1 to ANCHO do
 	begin
 		for j := 1 to ALTO do
 		begin
+			GotoXY(5 * i, 5 * j);
 			write(' ', Tablero[i,j], ' ');	
 		end;
 		writeln();
 	end;
+
+	writeln(); writeln(x);
 end;
 
 procedure draw;
@@ -50,7 +81,7 @@ end;
 
 procedure update;
 begin
-	
+	KeyScan();
 end;
 
 begin
